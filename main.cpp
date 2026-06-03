b0b2e9acd828f8824ce41f6aba8ff852a553f24d
wizdom0 <cmenchion15@gmail.com>
2026-06-02 23:45:18 -0400

    Reformat
    Changed indents. Added WATER cell type to Cell enum.

--- main.cpp
+++ main.cpp
@@ -12,65 +12,91 @@ unsigned int scale = 10;
 unsigned int gridWidth = winWidth / scale;
 unsigned int gridHeight = winHeight / scale;
 
-enum class Cell : uint8_t { Empty, Sand };
+enum class Cell : uint8_t
+{
+    EMPTY,
+    SAND,
+    WATER
+};
 
-void UpdateCells(std::vector<Cell> &grid) {
-    for (int i = grid.size() - 1; i >= 0; i--) {
-        switch (grid[i]) {
-        case Cell::Sand: {
-            int x = i % gridWidth;
-            if (i + (int)gridWidth >= (int)grid.size())
+void UpdateCells(std::vector<Cell> &grid)
+{
+    for (int i = grid.size() - 1; i >= 0; i--)
+    {
+        switch (grid[i])
+        {
+            case Cell::EMPTY:
                 break;
-            if (grid[i + gridWidth] == Cell::Empty) {
-                std::swap(grid[i], grid[i + gridWidth]); // Below
-            } else if (x > 0 && grid[i + gridWidth - 1] == Cell::Empty) {
-                std::swap(grid[i], grid[i + gridWidth - 1]); // Below left
-            } else if (x < (int)gridWidth - 1 && grid[i + gridWidth + 1] == Cell::Empty) {
-                std::swap(grid[i], grid[i + gridWidth + 1]); // Below Right
+            case Cell::SAND:
+            {
+                int x = i % gridWidth;
+                if (i + (int)gridWidth >= (int)grid.size())
+                    break;
+                if (grid[i + gridWidth] == Cell::EMPTY)
+                {
+                    std::swap(grid[i], grid[i + gridWidth]); // Below
+                }
+                else if (x > 0 && grid[i + gridWidth - 1] == Cell::EMPTY)
+                {
+                    std::swap(grid[i], grid[i + gridWidth - 1]); // Below left
+                }
+                else if (x < (int)gridWidth - 1 && grid[i + gridWidth + 1] == Cell::EMPTY)
+                {
+                    std::swap(grid[i], grid[i + gridWidth + 1]); // Below Right
+                }
+                break;
             }
-            break;
+            case Cell::WATER:
+            {
+                int x = i % gridWidth;
+            }
         }
-        case Cell::Empty:
-            break;
+    }
+}
+void DrawCells(const std::vector<Cell> &grid)
+{
+    for (size_t i = 0; i < grid.size(); i++)
+    {
+        switch (grid[i])
+        {
+            case Cell::SAND:
+            {
+                unsigned int xPos = i % gridWidth;
+                unsigned int yPos = i / gridWidth;
+
+                DrawRectangle(xPos * scale, yPos * scale, scale, scale, YELLOW);
+                break;
+            }
+            case Cell::EMPTY:
+                break;
         }
     }
 }
 
-void DrawCells(const std::vector<Cell> &grid) {
-    for (size_t i = 0; i < grid.size(); i++) {
-        switch (grid[i]) {
-        case Cell::Sand: {
-            unsigned int xPos = i % gridWidth;
-            unsigned int yPos = i / gridWidth;
-
-            DrawRectangle(xPos * scale, yPos * scale, scale, scale, YELLOW);
-            break;
-        }
-        case Cell::Empty:
-            break;
-        }
-    }
-}
-
-void HandleInput(std::vector<Cell> &grid) {
-    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+void HandleInput(std::vector<Cell> &grid)
+{
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+    {
         Vector2 mousePos = GetMousePosition();
         int mouseGridPosX = (int)mousePos.x / scale;
         int mouseGridPosY = (int)mousePos.y / scale;
 
         if (mouseGridPosX >= 0 && mouseGridPosX < (int)gridWidth && mouseGridPosY >= 0 &&
-            mouseGridPosY < (int)gridHeight) {
+            mouseGridPosY < (int)gridHeight)
+        {
             // Reverse formula to get index based on position
             int index = mouseGridPosY * gridWidth + mouseGridPosX;
 
-            if (grid[index] == Cell::Empty) {
-                grid[index] = Cell::Sand;
+            if (grid[index] == Cell::EMPTY)
+            {
+                grid[index] = Cell::SAND;
             }
         }
     }
 }
 
-int main() {
+int main()
+{
     InitWindow(winWidth, winHeight, "Cascade | Interactive Cell Simulator");
     SetTargetFPS(simFrameRate);
 
@@ -78,7 +104,8 @@ int main() {
     std::vector<Cell> grid(gridWidth * gridHeight);
 
     // Game loop
-    while (!WindowShouldClose()) {
+    while (!WindowShouldClose())
+    {
         UpdateCells(grid);
         HandleInput(grid);
 
